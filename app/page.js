ee48d1b846d99fcce1759b2ed0518a4021d95f52
ArifB <arif.bagde@somaiya.edu>
2026-01-28 00:45:36 +0530

    initial commit

---
+++ app/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-start justify-center font-sans">
+    <h1>Home Page</h1>
+    </div>
+  );
+}
